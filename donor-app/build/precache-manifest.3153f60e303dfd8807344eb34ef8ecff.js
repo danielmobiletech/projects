e6e7f7cb5c81c720b2b03766c136a8fb6ba5c337
Daniel <danielmobiletech@gmail.com>
2020-06-22 13:27:32 -0700

    donor-app
    this a donor-app test

---
+++ donor-app/build/precache-manifest.3153f60e303dfd8807344eb34ef8ecff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3433c174e4232f0367435d77b11b4fd9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "51c25c097b3f3efd6729",
+    "url": "/static/css/main.56b0d637.chunk.css"
+  },
+  {
+    "revision": "49a86630e28eaf5a5bfb",
+    "url": "/static/js/2.facbb189.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.facbb189.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51c25c097b3f3efd6729",
+    "url": "/static/js/main.098e549d.chunk.js"
+  },
+  {
+    "revision": "c924f95b9e22adb9fbda",
+    "url": "/static/js/runtime-main.4bda17de.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
